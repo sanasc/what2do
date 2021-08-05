7c933382eb6bc6bceea0e728da11afb7e27868db
Irene W <i.lalinw@gmail.com>
2021-08-04 17:59:55 -0700

    connected initial firestore data to CurrentList.js (preliminary display of documents)

--- app/src/Components/CurrentList.js
+++ app/src/Components/CurrentList.js
@@ -1,24 +1,60 @@
-import React from 'react'
+import React from 'react';
+import firebase from '../firebase';
 
 class CurrentList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      entries: [],
+      voteCount: [],
+      isLoaded: false
+    }
   }
 
   render() {
-    var entries = [];
-      for (let [key, value] of this.props.items) {
-        entries = entries.concat([key + ": " + value, <br/>]);
+    
+    if (!this.state.isLoaded) {
+      var db = firebase.firestore().collection("sessions").doc("n4JhCl5XDul2rGHAlJln");
+      db.collection("items").orderBy("count", "desc").get().then((querySnapshot) => {
+        var localEntries = [];
+        var localVoteCount = []; 
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data().count);
+            localEntries = localEntries.concat(doc.id);
+            localVoteCount = localVoteCount.concat(doc.data().count);
+        });
+        this.setState({
+          entries: localEntries,
+          voteCount: localVoteCount,
+          isLoaded: true
+        })
+      });
+      return (
+        <div>
+          <p>
+            Current Movies:
+            <br />
+            Loading...
+            
+          </p>
+        </div>
+      )
+    } else {
+      //   for (let [key, value] of this.props.items) {
+      //     entries = entries.concat([key + ": " + value, <br/>]);
+      // }
+      return (
+        <div>
+          <p>
+            Current Movies:
+            <br />
+            {this.state.entries}
+            
+          </p>
+        </div>
+      )
     }
-    return (
-      <div>
-        <p>
-          Current Movies:
-          <br />
-          {entries}
-        </p>
-      </div>
-    )
   }
 }
 
