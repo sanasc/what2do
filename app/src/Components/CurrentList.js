b2e0ed4c24d580eb562e467a05647978606aeb4b
Sana Suse <sana96@uw.edu>
2021-08-09 18:00:45 -0700

    Update CurrentList.js
    CurrentList updates with new added items
    used ComponentDidMount()

--- app/src/Components/CurrentList.js
+++ app/src/Components/CurrentList.js
@@ -11,45 +11,38 @@ class CurrentList extends React.Component {
     }
   }
 
-  render() {
+  componentDidMount() {
     var db = firebase.firestore().collection("sessions").doc("n4JhCl5XDul2rGHAlJln");
-    if (!this.state.isLoaded) {
-      db.collection("items").orderBy("count", "desc").get().then((querySnapshot) => {
-        var localEntries = [];
-        var localVoteCount = []; 
-        querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data().count);
-            localEntries = localEntries.concat(doc.id);
-            localVoteCount = localVoteCount.concat(doc.data().count);
-        });
-        this.setState({
-          entries: localEntries,
-          voteCount: localVoteCount,
-          isLoaded: true
-        })
+    db.collection("items").orderBy("count", "desc").onSnapshot((querySnapshot) => {
+      var localEntries = [];
+      var localVoteCount = [];
+      querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, " => ", doc.data().count);
+          localEntries = localEntries.concat(doc.id);
+          localVoteCount = localVoteCount.concat(doc.data().count);
       });
+      this.setState({
+        entries: localEntries,
+        voteCount: localVoteCount,
+        isLoaded: true
+      })
+    });
+  }
 
+  render() {
+    if (!this.state.isLoaded) {
       return (
         <div>
           <p>
             Current Movies:
             <br />
             Loading...
-            
+
           </p>
         </div>
       )
     } else {
-      // db.collection("items").orderBy("count", "desc").get().then(querySnapshot => {
-      //   let changes = querySnapshot.docChanges();
-      //   console.log("Made it into the snapshot portion of the else statement");
-      //   if (changes[0].type == 'added' || changes[0].type == 'removed') {
-      //     this.setState({isLoaded: false});
-      //     console.log("isLoaded is false!");
-      //   }
-      // })
-
       var displayItems = [];
       for (var i = 0; i < this.state.entries.length; i++) {
         displayItems.push(<li>{this.state.entries[i]} - {this.state.voteCount[i]} votes </li>);
