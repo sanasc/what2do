e09d847135a6af4861bc209083b6b740cce76b90
Sana Suse <sana96@uw.edu>
2021-08-11 17:30:50 -0700

    Update CurrentList.js
    Delete item entries!

--- app/src/Components/CurrentList.js
+++ app/src/Components/CurrentList.js
@@ -5,11 +5,13 @@ class CurrentList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      map: [],
       entries: [],
       voteCount: [],
       didVote: [],
       isLoaded: false
     }
+    this.deleteVote = this.deleteVote.bind(this);
   }
 
   componentDidMount() {
@@ -18,6 +20,7 @@ class CurrentList extends React.Component {
       var localEntries = [];
       var localVoteCount = [];
       var localUserVotes = [];
+
       querySnapshot.forEach((doc) => {
           // doc.data() is never undefined for query doc snapshots
           console.log(doc.id, " => ", doc.data().count);
@@ -26,11 +29,32 @@ class CurrentList extends React.Component {
           localUserVotes = localUserVotes.concat(doc.data().votes.includes(this.props.username));
       });
       this.setState({
+
         entries: localEntries,
         voteCount: localVoteCount,
         didVote: localUserVotes,
         isLoaded: true
       })
+    });
+  }
+
+  deleteVote(e) {
+    console.log(e.target.id);
+    var db = firebase.firestore().collection("sessions").doc("n4JhCl5XDul2rGHAlJln");
+
+    db.collection("items").doc(e.target.id).update({
+      count: firebase.firestore.FieldValue.increment(-1),
+      votes: firebase.firestore.FieldValue.arrayRemove(this.props.username)
+    });
+
+    db.collection("items").doc(e.target.id).get().then((doc) => {
+      if (doc.data().count == 0) {
+        db.collection("items").doc(e.target.id).delete().then(() => {
+          console.log("Document successfully deleted!");
+        }).catch((error) => {
+          console.error("Error removing document: ", error);
+        });
+      }
     });
   }
 
@@ -49,7 +73,8 @@ class CurrentList extends React.Component {
     } else {
       var displayItems = [];
       for (var i = 0; i < this.state.entries.length; i++) {
-        displayItems.push(<li>{ this.state.entries[i] }{ this.state.didVote[i] ? "*" : "" } -  { this.state.voteCount[i] } votes </li>);
+        var itemName = this.state.entries[i];
+        displayItems.push(<li id={i}>{ itemName } - { this.state.voteCount[i] } votes { this.state.didVote[i] ? <button id={itemName} onClick={this.deleteVote} >Delete</button> : "" } </li>);
       }
 
       return (
